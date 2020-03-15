c22875a094b11bb3414ef649f6243c12be817e6c
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-03-15 13:36:21 -0400

    Create IsSheGonnaSurvive.js

---
+++ IsSheGonnaSurvive.js
@@ -0,0 +1,10 @@
+/*
+A hero is on his way to the castle to complete her mission. However, she's been told that the castle is surrounded with a couple of powerful dragons! Each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets she should carry.. Assuming she's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will she survive?
+
+Return True if yes, False otherwise :)
+*/
+
+function hero(bullets, dragons){
+//Get Coding!
+return (bullets/2 >=dragons)? true : false
+}
