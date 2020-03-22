67c9a7f40589d3473d6eb24fcef21c9d7e039a90
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-03-22 14:49:49 -0400

    Create CalculateAverage.js

---
+++ CalculateAverage.js
@@ -0,0 +1,9 @@
+/*
+Write function avg which calculates average of numbers in given list.
+*/
+
+function find_average(array) {
+const makeSmaller = (a, b) => {return a+b};
+
+return array.reduce(makeSmaller)/array.length
+}
