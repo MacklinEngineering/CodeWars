dffdf677b0b8a907f0ad1fe5aa485b5af21c10d6
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-04-17 14:36:21 -0400

    Delete RepeatString.js

--- RepeatString.js
+++
@@ -1,8 +0,0 @@
-// Write a function called repeatString which repeats the given String src exactly count times.
-
-function repeatStr (n, s) {
-  string = ""
-  if (n >0);{
-    return s.repeat(n);
-  }
-}
