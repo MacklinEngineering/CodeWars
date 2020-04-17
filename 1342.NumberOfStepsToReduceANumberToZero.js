07f6c29de25420fb5ae5aef3214332e01045797a
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-04-17 10:16:14 -0400

    Create 1342.NumberOfStepsToReduceANumberToZero.js

---
+++ 1342.NumberOfStepsToReduceANumberToZero.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps  = function(num) {
+const divide = a => a / 2;
+const minus = a => a - 1;
+
+let steps = 0;
+
+do {
+  if (num % 2 === 0) {
+    num = divide(num);
+  } else {
+    num = minus(num);
+  }
+
+  steps++;
+} while (num !==0);
+return steps
+
+}
