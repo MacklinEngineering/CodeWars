0999c1fbc718faea5ff6df14b09d2de4840b1071
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-06-28 09:40:35 -0400

    Create 912SortAnArray.js

---
+++ 912SortAnArray.js
@@ -0,0 +1,34 @@
+/*
+912. Sort an Array
+Medium
+
+415
+
+265
+
+Add to List
+
+Share
+Given an array of integers nums, sort the array in ascending order.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+ 
+
+
+*/
+
+
+var sortArray = function(nums) {
+    return nums.sort(function (a, b){
+        return a - b
+    })
+};
