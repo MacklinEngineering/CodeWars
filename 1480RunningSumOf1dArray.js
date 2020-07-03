a335799f896611364606978cffa350aaa4c7c499
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-07-03 09:57:19 -0400

    Create  1480RunningSumOf1dArray.js

---
+++ 1480RunningSumOf1dArray.js
@@ -0,0 +1,44 @@
+/**
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+ * @param {number[]} nums
+ * @return {number[]}
+ //take first thing
+ //add the value at index[0]+1
+ //append to a new array
+ //continue looping  
+ //if no more index +1, stop
+ //push first number into new array
+ */
+var runningSum = function(nums) {
+    let arr=[]
+    let total = 0
+    // arr.push(nums[0])
+    for (let i=0; i<nums.length; i++){
+        total+=nums[i]
+        
+        arr.push(total)
+    }
+    
+    return arr
+};
